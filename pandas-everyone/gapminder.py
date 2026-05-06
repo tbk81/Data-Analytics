c26709bf28703a4c0bec50a6da9a06811c5c1a03
MBP <dev@tbk81.com>
2026-05-06 15:26:03 -0700

    gapminder

--- pandas-everyone/gapminder.py
+++ pandas-everyone/gapminder.py
@@ -2,7 +2,22 @@ import pandas as pd
 
 df = pd.read_csv('./data/gapminder.tsv', sep='\t')
 # print(df)
+
+# check data type ie a dataframe
 print(type(df))
+
+# retrieves the number of rows and columns
 print(df.shape)
 
+# get column names
+print(df.columns)
+
+# get the dtype of each column
+print(df.dtypes)
+
+# get more information about our data
+print(df.info())
+
+# show the first 5 observations
+print(df.head())
 
