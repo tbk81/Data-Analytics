7bccdc924311b9412ec75b5e308546c0995917a6
MBP <dev@tbk81.com>
2026-05-07 16:55:46 -0700

    gapminder

--- pandas-everyone/gapminder.py
+++ pandas-everyone/gapminder.py
@@ -35,7 +35,25 @@ df = pd.read_csv('./data/gapminder.tsv', sep='\t')
 
 # get the first row
 print(df.iloc[0])
+# for i in range(10):
+#     print(df.iloc[i])
 
+# get last row
+num_of_rows = df.shape[0]
+last_row_index = num_of_rows - 1
+print(df.iloc[last_row_index])
 
+# another way to get the last row
+print(df.tail(n=1))
+
+# get the last row of data in different ways
+subset_loc = df.loc[0]
+subset_head = df.head(n=1)
+
+# type using loc of 1 row
+print(type(subset_loc))
+
+# type of using head of 1 row
+print(type(subset_head))
 
 
