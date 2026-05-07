86ede982872625fe6aa11df3ea4e5cfb41c02c3f
MBP <dev@tbk81.com>
2026-05-07 16:22:11 -0700

    gapminder

--- pandas-everyone/gapminder.py
+++ pandas-everyone/gapminder.py
@@ -4,20 +4,38 @@ df = pd.read_csv('./data/gapminder.tsv', sep='\t')
 # print(df)
 
 # check data type ie a dataframe
-print(type(df))
+# print(type(df))
 
 # retrieves the number of rows and columns
-print(df.shape)
+# print(df.shape)
 
 # get column names
-print(df.columns)
+# print(df.columns)
 
 # get the dtype of each column
-print(df.dtypes)
+# print(df.dtypes)
 
 # get more information about our data
-print(df.info())
+# print(df.info())
 
 # show the first 5 observations
-print(df.head())
+# print(df.head())
+
+# just get the country column and save it to its own variable
+# country_df = df['country']
+# print(country_df.head())
+# print(country_df.tail())
+
+# Looking at country, continent, and year
+# subset = df[['country', 'continent', 'year']]
+# print(subset.head())
+
+# using dot notation
+# print(df.country.head())
+
+# get the first row
+print(df.iloc[0])
+
+
+
 
